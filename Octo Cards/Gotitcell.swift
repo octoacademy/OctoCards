c4b066c74bca42e2bfc506ebdd5fb6174c7651ff
Linda Ho <linda@Lindas-MacBook-Pro.local>
2017-05-12 05:22:41 -0700

    Initial Commit

---
+++ Octo Cards/Gotitcell.swift
@@ -0,0 +1,24 @@
+//
+//  Gotitcell.swift
+//  Octo Cards
+//
+//  Created by Linda Ho on 5/11/17.
+//  Copyright © 2017 OctoAcademy. All rights reserved.
+//
+
+import UIKit
+
+class Gotitcell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
